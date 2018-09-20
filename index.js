b603a27beb01bbf6387a99566c52f0fe69ce2d97
suvir@me.com <suvir@me.com>
2018-09-20 23:13:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ function myAnimal() {
   return animal
 }
 
-function yourAnimal(animal) {
+function yourAnimal(anotherAnimal) {
   // How can we make sure that this function
   // and the above function both pass?
   // P.S.: You can't just hard-code 'cat' below
-  return animal
+  return anotherAnimal;
 }
 
 function add2(n) {
